9dbf3e598c76ca95e03cfb8fc0112afcfb1a18d5
Wiktor Flis <wikflis@gmail.com>
2020-05-03 20:43:59 +0200

    Mongodb

--- test/index.js
+++ test/index.js
@@ -1,24 +1,22 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require("mongodb")
 
-const user = 'nodebb-pl'
+const user = "nodebb-pl"
 const pass = process.env.NODEBB_PL_PASS
 
-const url = `mongodb://${user}:${pass}@mongo:27017?authMechanism=DEFAULT&authSource=db`
+const url = `mongodb://${user}:${pass}@172.16.238.2`
 
 async function main() {
-    console.log("Connecting to " + url)
-    const client = new MongoClient(url);
+  console.log("Connecting to " + url)
+  const client = new MongoClient(url)
 
-    try {
-        await client.connect();
-        console.log("connected") 
-        let res = await listDatabases(client);
-        console.log({"databases": res})
-     
-    } catch (e) {
-        console.error(e);
-    }
-
+  try {
+    await client.connect()
+    console.log("connected")
+    let res = await listDatabases(client)
+    console.log({ databases: res })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 main()
